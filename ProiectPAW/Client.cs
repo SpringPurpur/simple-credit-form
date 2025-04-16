ceae643d078b9d0c8a3128a5c52a86b72c0e0556
SpringPurpur <vaceffv@gmail.com>
2025-04-16 11:05:44 +0300

    feat: implemented core functionality for client and credit management
    -Added client and credit form
    -Implemented validation and UI navigation
    -Enabled serialization and file export locally
    -Enabled report generation for clients' contracted credits

---
+++ ProiectPAW/Client.cs
@@ -0,0 +1,43 @@
+﻿namespace ProiectPAW
+{
+    [Serializable]
+    public class Client : IComparable<Client>
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string CNP { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public float CreditScore { get; set; }
+
+        public List<Credit> Credits { get; set; } = [];
+
+        public Client(string LastName, string FirstName, string CNP, string Email, string PhoneNumber, float CreditScore)
+        {
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.CNP = CNP;
+            this.Email = Email;
+            this.PhoneNumber = PhoneNumber;
+            this.CreditScore = CreditScore;
+        }
+
+        public override string? ToString()
+        {
+            string format = this.CNP + " - " + this.LastName + " " + this.FirstName;
+            return format;
+        }
+
+        public int CompareTo(Client? other)
+        {
+            if (other == null) return 1;
+            return this.CNP.CompareTo(other.CNP);
+        }
+
+        public string FullInfo()
+        {
+            string format = "Nume: " + this.LastName + "\nPrenume: " + this.FirstName + "\nCNP: " + this.CNP + "\nEmail: " + this.Email + "\nNumar de telefon: " + this.PhoneNumber;
+            return format;
+        }
+    }
+}
